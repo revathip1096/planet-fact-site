4e2d49abf448904f0270faa9a16d7e267e650c9f
Revathi <Revathi092001@gmail.com>
2023-09-12 13:17:41 +0530

    updated mobile view

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,82 +1,127 @@
 "use client"
-import React from 'react'
-import {data} from '../../public/data.js'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import React, { useState } from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
- 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-  
-   <nav className="bg-white border-gray-200 dark:bg-gray-900 ">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6 ">
-    <a href="/" className="flex items-center">
-      
-      <span className="content-between self-center text-2xl whitespace-nowrap dark:text-white ml-[-70px] font-[antonio]
-    font-bold
-    text-2xl/4 ">THE PLANETS</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
-      </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default ">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 mr-[-60px]">
-      <li>
-  <a
-    href="/planets/mercury"
-    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500 font-family-[spartan] font-weight-700"
-    aria-current="page"
-  >
-    MERCURY
-  </a>
-  <span className="before:block before:h-[2px] before:w-[100%] before:bg-white before:absolute before:left-0 before:opacity-0 before:transition-all before:duration-[0.2s] hover:before:opacity-1"></span>
-</li>
-
-        <li>
-          <a href="/planets/venus" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">VENUS</a>
-        </li>
-        <li>
-          <a href="/planets/earth" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">EARTH</a>
-        </li>
-        <li>
-          <a href="/planets/mars" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">MARS</a>
-        </li>
-        <li>
-          <a href="/planets/jupiter" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">JUPITER</a>
-        </li>
-        <li>
-          <a href="/planets/saturn" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">SATURN</a>
-        </li>
-        <li>
-          <a href="/planets/uranus" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">URANUS</a>
-        </li>
-        <li>
-          <a href="/planets/neptune" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">NEPTUNE</a>
-        </li>
-      </ul>
+    <div className="fixed top-0 w-full z-10">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
+          <Link href="/" className="flex items-center">
+            <span className="text-2xl whitespace-nowrap dark:text-white font-[antonio] font-bold text-2xl/4">
+              THE PLANETS
+            </span>
+          </Link>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            onClick={toggleMenu}
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className={`w-5 h-5 ${isMenuOpen ? 'hidden' : 'block'}`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className={`w-full md:flex md:w-auto ${
+              isMenuOpen ? 'block' : 'hidden'
+            }`}
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  href="/planets/mercury"
+                  className="block py-2 pl-3 pr-4 text-blue-700 rounded md:text-white md:bg-transparent md:p-0 font-family-[spartan] font-weight-700"
+                >
+                  MERCURY
+                </Link>
+                <span className="before:block before:h-[2px] before:w-[100%] before:bg-white before:absolute before:left-0 before:opacity-0 before:transition-all before:duration-[0.2s] hover:before:opacity-1"></span>
+              </li>
+              <li>
+                <Link
+                  href="/planets/venus"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  VENUS
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/earth"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  EARTH
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/mars"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  MARS
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/jupiter"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  JUPITER
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/saturn"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  SATURN
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/uranus"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  URANUS
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/planets/neptune"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:text-white md:bg-transparent md:p-0 dark:text-white dark:hover:bg-gray-700"
+                >
+                  NEPTUNE
+                </Link>
+              </li>
+              {/* Add links to the other planets here */}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-{/*</Router>
-<Routes>
-<Route exact path='/' element={< planet-info />}></Route>
-<Route exact path='/venus' element={< planet-info />}></Route>
-<Route exact path='/earth' element={< planet-info />}></Route>
-<Route exact path='/mars' element={< planet-info />}></Route>
-<Route exact path='/jupiter' element={< planet-info />}></Route>
-<Route exact path='/saturn' element={< planet-info />}></Route>
-<Route exact path='/uranus' element={< planet-info />}></Route>
-<Route exact path='/neptune' element={< planet-info />}></Route>
-</Routes> */}
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
--- app/planets/[planet]/page.js
+++ app/planets/[planet]/page.js
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useState } from "react";
 import { FaExternalLinkSquareAlt } from "react-icons/fa";
 import Image from "next/image";
@@ -49,78 +48,92 @@ const Planet = ({ params }) => {
     <>
       <Navbar />
 
-      <div className="py-20 px-20">
-        <div className="flex">
-          <div className="w-9/12 flex justify-center items-center">
-            {/* Content for the left column */}
-            <img src={getSectionImage()} alt={planetData.name} />
-          </div>
-          <div className="w-3/12 p-4 mt-20 mr-20">
-            {/* Content for the right column */}
-            <h2 className="text-3xl font-semibold mb-4">
-              {planetData.name.toUpperCase()}
-            </h2>
-            {getSectionContent()}
-            <div className="flex text-l font-semibold mt-4">
-              <h3 className=" ">Source:</h3>
-              <div className="flex ">
-                <a
-                  href={planetData.overview.source}
-                  className="underline ml-2"
-                  target="_blank"
-                >
-                  Wikipedia
-                </a>
-                <FaExternalLinkSquareAlt className="mt-[4px] ml-2" />
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <button className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[70px] text-left ${activeSection === "overview" ? "bg-gray-400" : ""}`} onClick={() => handleSectionChange("overview")}>
-                01 &nbsp;&nbsp;OVERVIEW
-              </button>
-              <button className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[10px] text-left ${activeSection === "structure" ? "bg-gray-400" : ""}`} onClick={() => handleSectionChange("structure")}>
-                02 &nbsp;&nbsp;INTERNAL STRUCTURE
-              </button>
-              <button className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[10px] text-left ${activeSection === "geology" ? "bg-gray-400" : ""}`} onClick={() => handleSectionChange("geology")}>
-                03 &nbsp;&nbsp;SURFACE GEOLOGY
-              </button>
+      <div className="p-4 lg:py-20 lg:px-20 lg:flex">
+        <div className="lg:w-9/12 lg:flex lg:justify-center lg:items-center">
+          {/* Content for the left column */}
+          <img
+            src={getSectionImage()}
+            alt={planetData.name}
+            className="max-w-full h-auto mt-4 lg:mt-8 "
+          />
+        </div>
+        <div className="lg:w-3/12 lg:p-4 lg:mt-20 lg:mr-20">
+          {/* Content for the right column */}
+          <h2 className="text-3xl font-semibold mb-4">
+            {planetData.name.toUpperCase()}
+          </h2>
+          {getSectionContent()}
+          <div className="flex text-l font-semibold mt-4">
+            <h3 className=" ">Source:</h3>
+            <div className="flex ">
+              <a
+                href={planetData.overview.source}
+                className="underline ml-2"
+                target="_blank"
+              >
+                Wikipedia
+              </a>
+              <FaExternalLinkSquareAlt className="mt-[4px] ml-2" />
             </div>
           </div>
+          <div className="flex flex-col">
+            <button
+              className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[70px] text-left ${activeSection === "overview" ? "bg-gray-400" : ""
+                }`}
+              onClick={() => handleSectionChange("overview")}
+            >
+              01 &nbsp;&nbsp;OVERVIEW
+            </button>
+            <button
+              className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[10px] text-left ${activeSection === "structure" ? "bg-gray-400" : ""
+                }`}
+              onClick={() => handleSectionChange("structure")}
+            >
+              02 &nbsp;&nbsp;INTERNAL STRUCTURE
+            </button>
+            <button
+              className={`bg-[#419DB9] text-[#ffffff] font-bold py-2 px-6 hover:bg-gray-400 mt-[10px] text-left ${activeSection === "geology" ? "bg-gray-400" : ""
+                }`}
+              onClick={() => handleSectionChange("geology")}
+            >
+              03 &nbsp;&nbsp;SURFACE GEOLOGY
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center gap-10 mt-[70px]">
-          <span className="block max-w-sm p-8 px-[60px] border border-white-200">
-            <h5 className="font-normal text-gray-700 dark:text-gray-400">
-              ROTATION TIME
-            </h5>
-            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {planetData.rotation}
-            </p>
-          </span>
-          <span className="block max-w-sm p-8 px-[60px] border border-white-200">
-            <h5 className="font-normal text-gray-700 dark:text-gray-400">
-              REVOLUTION TIME
-            </h5>
-            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {planetData.revolution}
-            </p>
-          </span>
-          <span className="block max-w-sm p-8 px-[60px] border border-white-200">
-            <h5 className="font-normal text-gray-700 dark:text-gray-400">
-              RADIUS
-            </h5>
-            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {planetData.radius}
-            </p>
-          </span>
-          <span className="block max-w-sm p-8 px-[60px] border border-white-200">
-            <h5 className="font-normal text-gray-700 dark:text-gray-400">
-              AVERAGE TEMP.
-            </h5>
-            <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {planetData.temperature}
-            </p>
-          </span>
-        </div>
+      </div>
+      <div className="lg:flex lg:justify-center lg:gap-10 lg:mt-20">
+        <span className="block max-w-sm p-8 px-[60px] border border-white-200">
+          <h5 className="font-normal text-gray-700 dark:text-gray-400">
+            ROTATION TIME
+          </h5>
+          <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {planetData.rotation}
+          </p>
+        </span>
+        <span className="block max-w-sm p-8 px-[60px] border border-white-200">
+          <h5 className="font-normal text-gray-700 dark:text-gray-400">
+            REVOLUTION TIME
+          </h5>
+          <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {planetData.revolution}
+          </p>
+        </span>
+        <span className="block max-w-sm p-8 px-[60px] border border-white-200">
+          <h5 className="font-normal text-gray-700 dark:text-gray-400">
+            RADIUS
+          </h5>
+          <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {planetData.radius}
+          </p>
+        </span>
+        <span className="block max-w-sm p-8 px-[60px] border border-white-200">
+          <h5 className="font-normal text-gray-700 dark:text-gray-400">
+            AVERAGE TEMP.
+          </h5>
+          <p className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {planetData.temperature}
+          </p>
+        </span>
       </div>
     </>
   );
